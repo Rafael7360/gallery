bdddbc94c498d100ecf9dcd2381d9501b46bc314
Rafael <rafael.gomes7360@hotmail.com>
2016-09-17 13:20:50 -0300

    rascunho itvimg

---
+++ atvimg.js
@@ -0,0 +1,103 @@
+function atvImg(){
+
+	var d = document,
+		de = d.documentElement,
+		bd = d.getElementsByTagName('body')[0],
+		htm = d.getElementsByTagName('html')[0],
+		win = window,
+		imgs = d.querySelectorAll('.atvImg'),
+		totalImgs = imgs.length,
+		supportsTouch = 'ontouchstart' in win || navigator.msMaxTouchPoints;
+
+	if(totalImgs <= 0){
+		return;
+	}
+
+	// build HTML
+	for(var l=0;l<totalImgs;l++){
+
+		var thisImg = imgs[l],
+			layerElems = thisImg.querySelectorAll('.atvImg-layer'),
+			totalLayerElems = layerElems.length;
+
+		if(totalLayerElems <= 0){
+			continue;
+		}
+
+		while(thisImg.firstChild) {
+			thisImg.removeChild(thisImg.firstChild);
+		}
+	
+		var containerHTML = d.createElement('div'),
+			shineHTML = d.createElement('div'),
+			shadowHTML = d.createElement('div'),
+			layersHTML = d.createElement('div'),
+			layers = [];
+
+		thisImg.id = 'atvImg__'+l;
+		containerHTML.className = 'atvImg-container';
+		shineHTML.className = 'atvImg-shine';
+		shadowHTML.className = 'atvImg-shadow';
+		layersHTML.className = 'atvImg-layers';
+
+		for(var i=0;i<totalLayerElems;i++){
+			var layer = d.createElement('div'),
+				imgSrc = layerElems[i].getAttribute('data-img');
+
+			layer.className = 'atvImg-rendered-layer';
+			layer.setAttribute('data-layer',i);
+			layer.style.backgroundImage = 'url('+imgSrc+')';
+			layersHTML.appendChild(layer);
+
+			layers.push(layer);
+		}
+
+		containerHTML.appendChild(shadowHTML);
+		containerHTML.appendChild(layersHTML);
+		containerHTML.appendChild(shineHTML);
+		thisImg.appendChild(containerHTML);
+
+		var w = thisImg.clientWidth || thisImg.offsetWidth || thisImg.scrollWidth;
+		thisImg.style.transform = 'perspective('+ w*3 +'px)';
+
+		if(supportsTouch){
+			win.preventScroll = false;
+
+	        (function(_thisImg,_layers,_totalLayers,_shine) {
+				thisImg.addEventListener('touchmove', function(e){
+					if (win.preventScroll){
+						e.preventDefault();
+					}
+					processMovement(e,true,_thisImg,_layers,_totalLayers,_shine);		
+				});
+	            thisImg.addEventListener('touchstart', function(e){
+	            	win.preventScroll = true;
+					processEnter(e,_thisImg);		
+				});
+				thisImg.addEventListener('touchend', function(e){
+					win.preventScroll = false;
+					processExit(e,_thisImg,_layers,_totalLayers,_shine);		
+				});
+	        })(thisImg,layers,totalLayerElems,shineHTML);
+	    } else {
+	    	(function(_thisImg,_layers,_totalLayers,_shine) {
+				thisImg.addEventListener('mousemove', function(e){
+					processMovement(e,false,_thisImg,_layers,_totalLayers,_shine);		
+				});
+	            thisImg.addEventListener('mouseenter', function(e){
+					processEnter(e,_thisImg);		
+				});
+				thisImg.addEventListener('mouseleave', function(e){
+					processExit(e,_thisImg,_layers,_totalLayers,_shine);		
+				});
+	        })(thisImg,layers,totalLayerElems,shineHTML);
+	    }
+	}
+
+	function processMovement(e, touchEnabled, elem, layers, totalLayers, shine){
+
+		var bdst = bd.scrollTop || htm.scrollTop,
+			bdsl = bd.scrollLeft,
+			pageX = (touchEnabled)? e.touches[0].pageX : e.pageX,
+			pageY = (touchEnabled)? e.touches[0].pageY : e.pageY,
+			offsets = elem.getBoundingClientRect(),
